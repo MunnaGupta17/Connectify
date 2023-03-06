113dabbbc8d48b4d0ad6e0ac205a1eeedc679dbd
MunnaGupta17 <mgabd17@gmail.com>
2023-03-06 13:39:54 +0530

    basic package creation are done

---
+++ Backend/Rest-API_Connectify/src/main/java/com/connectify/RestApiConnectifyApplication.java
@@ -0,0 +1,13 @@
+package com.connectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiConnectifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiConnectifyApplication.class, args);
+	}
+
+}
